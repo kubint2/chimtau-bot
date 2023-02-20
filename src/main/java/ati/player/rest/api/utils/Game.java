8f9c8dd23af27f13963b6645e066b8995eb4e3a1
Binhlv <vanbinhluong92@gmail.com>
2023-02-20 23:35:53 +0700

    'add'

--- src/main/java/ati/player/rest/api/utils/Game.java
+++ src/main/java/ati/player/rest/api/utils/Game.java
@@ -1,22 +1,31 @@
 package ati.player.rest.api.utils;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
+
 import ati.player.rest.api.entity.Coordinate;
+import ati.player.rest.api.response.GameTurnResult;
 
 public class Game {
     private int[][] board;
-    private int boardWidth;
-    private int boardHeight;
-    private int shotsPerTurn;
+    private int boardWidth = 20;
+    private int boardHeight = 8;
+    private int shotsPerTurn = 1;
+    private int hitCount = 0;
     
-
     private int previousHitX;
     private int previousHitY;
+    
+    private Boolean vertical = null;
+    List<Coordinate> hitList = new ArrayList<>();
+    Coordinate previousHit = new Coordinate (1,1);
     
     private List<Shot> previousShots = new ArrayList<>();
 
@@ -28,15 +37,144 @@ public class Game {
         
     }
 
-//    public boolean fire(int x, int y) {
-//        if (board[y][x] == 0) {
-//            board[y][x] = 1;
-//            return false;
-//        } else {
-//            board[y][x] = 2;
-//            return true;
-//        }
-//    }
+    public void getGameTurnResult() {
+    	GameTurnResult response = new GameTurnResult();
+    	int shotTurn = 2;
+    	List<int[]> coordinates = new ArrayList<>();
+
+    	List<Coordinate> shotsTurn = getshotsTurn();
+    	for (Coordinate coordinate : shotsTurn) {
+    		coordinates.add(new int[]{coordinate.getX(),coordinate.getY()} );
+		}
+
+    	response.setCoordinates(coordinates);
+    	
+    	System.out.println(JsonUtil.objectToJson(response));
+    }
+    
+    
+    public List<Coordinate> getshotsTurn() {
+    	List<Coordinate> shotsTurn = new ArrayList<>();
+    	
+    	if(hitList.size() == 0) {
+    		// Random shot
+    	} else if (hitList.size() == 1) {
+    		// Shot neightBour previousHit
+
+    	} else if (hitList.size() == 2) {
+    		// Shot vertical or Not
+            Coordinate first = hitList.get(0);
+            Coordinate second = hitList.get(1);
+            
+            List<Coordinate> neightBour2point = new ArrayList<>();
+            // check vertical
+            if (first.getY() == second.getY()) {
+            	neightBour2point = getNeightBourTypeA(hitList, true);
+
+            } else if (first.getX() == second.getX()) {
+            	neightBour2point = getNeightBourTypeA(hitList, false);
+            }
+
+        	if (CollectionUtils.isNotEmpty(neightBour2point)) {
+        		return neightBour2point;
+        	} else {
+        		// Shot neightBours hit list
+        	}
+
+    	} else if (hitList.size() == 3) {
+            Coordinate first = hitList.get(0);
+            Coordinate second = hitList.get(1);
+            Coordinate third = hitList.get(2);
+            
+            List<Coordinate> neightBour3point = new ArrayList<>();
+            // check vertical
+            if (first.getY() == second.getY() && first.getY() == third.getY()) {
+            	vertical = true;
+            	// lấy 1 điểm trong 2 điểm đầu và cuối để shot
+            	neightBour3point = getNeightBourTypeA(hitList, true);
+            	
+            	
+            	
+            } else if (first.getX() == second.getX() && first.getX() == third.getX()) {
+            	vertical = false;
+            	neightBour3point = getNeightBourTypeA(hitList, false);
+            } else {
+            	vertical = null;
+            	// 3 điểm không thẳng hàng -> tìm điểm góc vuông
+            	Coordinate coordinateFourth = findFourthCoordinate(first, second, third);
+            	// Check valid coordinateFourth
+            	if (board[coordinateFourth.getX()][coordinateFourth.getY()] == 0) {
+            		neightBour3point.add(coordinateFourth);
+            	}
+            }
+            
+        	if (CollectionUtils.isNotEmpty(neightBour3point)) {
+        		return neightBour3point;
+        	} else {
+        		// Shot neightBours hit list
+        	}
+    	}
+    	return shotsTurn;
+    }
+    
+    public List<Coordinate> getNeightBourTypeA(List<Coordinate> hitList, boolean vertical) {
+    	List<Coordinate> neightBourTypeA = new ArrayList<>();
+    	Coordinate min;
+    	Coordinate max;
+		if (vertical) {
+			Coordinate minCoordinateRow = hitList.stream().min(Comparator.comparing(Coordinate::getX))
+					.orElseThrow(NoSuchElementException::new);
+			Coordinate maxCoordinateRow = hitList.stream().max(Comparator.comparing(Coordinate::getX))
+					.orElseThrow(NoSuchElementException::new);
+
+			int minRow = minCoordinateRow.getX();
+			int maxRow = maxCoordinateRow.getX();
+			int col = minCoordinateRow.getY();
+			// validate
+			if ((minRow - 1) > 0 && board[minRow - 1][col] == 0) {
+				neightBourTypeA.add(new Coordinate(minRow - 1, col));
+			}
+			if ((maxRow + 1) < boardHeight && board[maxRow + 1][col] == 0) {
+				neightBourTypeA.add(new Coordinate(minRow - 1, col));
+			}
+		} else {
+			Coordinate minCoordinateCol = hitList.stream().min(Comparator.comparing(Coordinate::getY))
+					.orElseThrow(NoSuchElementException::new);
+			Coordinate maxCoordinateCol = hitList.stream().max(Comparator.comparing(Coordinate::getY))
+					.orElseThrow(NoSuchElementException::new);
+
+        	int minCol = minCoordinateCol.getY();
+        	int maxCol = maxCoordinateCol.getY();
+        	int row = minCoordinateCol.getX();
+        	// validate
+        	if ((minCol-1) > 0 && board[row][minCol-1] == 0) {
+        		neightBourTypeA.add(new Coordinate(row, minCol-1));
+        	}
+        	if ((maxCol+1) < boardWidth && board[row][maxCol+1] == 0) {
+        		neightBourTypeA.add(new Coordinate(row, maxCol+1));
+        	}
+		}    	
+    	return neightBourTypeA;
+    }
+    
+    
+    public static Coordinate findFourthCoordinate(Coordinate first, Coordinate second, Coordinate third) {
+        // Tìm tọa độ còn lại
+        int x4, y4;
+        if (first.getX() == second.getX()) {
+            // Trường hợp đường chéo chính nằm dọc
+            x4 = third.getX();
+            y4 = first.getY() + third.getY() - second.getY();
+        } else {
+            // Trường hợp đường chéo chính nằm ngang
+            x4 = first.getX() + third.getX() - second.getX();
+            y4 = third.getY();
+        }
+
+        // Tạo đối tượng Coordinate mới để lưu tọa độ của đỉnh còn lại
+        Coordinate fourth = new Coordinate(x4, y4);
+        return fourth;
+    }
 
     public List<int[]> getAdjacentCells(int x, int y) {
         List<int[]> adjacentCells = new ArrayList<int[]>();
--- src/main/java/ati/player/rest/api/utils/TestMain.java
+++ src/main/java/ati/player/rest/api/utils/TestMain.java
@@ -1,8 +1,13 @@
 package ati.player.rest.api.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ati.player.rest.api.entity.Coordinate;
+
 public class TestMain {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main1(String[] args) throws InterruptedException {
 		
 		while (true) {
 			Board board = new Board(20, 8);
@@ -22,12 +27,44 @@ public class TestMain {
 			board.addShip(new Ship("CV"));
 			board.placeShipsRandomly();
 			board.print();
-			
 			Thread.sleep(2000);
 		}
-
-		
 	}
 	
+	public static void main(String[] args) {
+		List<Coordinate> hitList = new ArrayList<>();
+		hitList.add(new Coordinate(1, 1));
+		hitList.add(new Coordinate(1, 2));
+		hitList.add(new Coordinate(2, 2));	
+	
+		Coordinate fourthCoordinate = findFourthCoordinate(hitList);
+        // In tọa độ của đỉnh còn lại
+        System.out.println("(" + fourthCoordinate.getX() + ", " + fourthCoordinate.getY() + ")");
+    }
+	
+    public static Coordinate findFourthCoordinate(List<Coordinate> coordinates) {
+        // Lấy tọa độ các đỉnh đã biết
+        Coordinate first = coordinates.get(0);
+        Coordinate second = coordinates.get(1);
+        Coordinate third = coordinates.get(2);
+
+        // Tìm tọa độ còn lại
+        int x4, y4;
+        if (first.getX() == second.getX()) {
+            // Trường hợp đường chéo chính nằm dọc
+            x4 = third.getX();
+            y4 = first.getY() + third.getY() - second.getY();
+        } else {
+            // Trường hợp đường chéo chính nằm ngang
+            x4 = first.getX() + third.getX() - second.getX();
+            y4 = third.getY();
+        }
+
+        // Tạo đối tượng Coordinate mới để lưu tọa độ của đỉnh còn lại
+        Coordinate fourth = new Coordinate(x4, y4);
+        return fourth;
+    }
+	
+
 	
 }
